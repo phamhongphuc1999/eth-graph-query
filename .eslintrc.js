972df94a122f2d36e120ffdf54b63c8e012b91b8
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2023-06-09 16:41:05 +0700

    feat(init): init the first package

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', 'prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    quotes: ['error', 'single'],
+    semi: ['warn', 'always'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-named-as-default': 'error',
+    'import/no-unresolved': 'warn',
+    'no-console': ['warn', { allow: ['debug', 'warn', 'error'] }],
+    'no-debugger': 'warn',
+  },
+  settings: {
+    'import/ignore': ['node_modules'],
+    'import/resolver': {
+      alias: {
+        map: [
+          ['src', './src/'],
+          ['public', './public/'],
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+<h1 align="center">
+Eth graph query
+</h1>
+
+---
+
+### Installation
+
+```shell
+npm install eth-graph-query
+
+```
+
+---
+
+### Usage
